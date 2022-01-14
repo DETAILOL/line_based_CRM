16dd9263a11813b83ae284b4ea20fb784c61b583
DETAILOL <johnson81826@gmail.com>
2022-01-14 15:06:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text + event.source.user_id + event.source.room_id))
+        TextSendMessage(text=event.message.text + event.source.user_id))
     
 
     
